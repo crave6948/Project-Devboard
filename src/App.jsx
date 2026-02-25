49360656138f2b2927d41a4a9fa3d330a2c0c808
Peraphat Samngamya <67160194@go.buu.ac.th>
2026-02-25 20:47:50 +0700

    feat: lab11 - static devboard components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import './App.css'
-import Navbar from './components/Navbar';
-import PostCard from './components/PostCard';
+import Navbar from "./components/Navbar";
+import PostList from "./components/PostList";
+import UserCard from "./components/UserCard";
 
 const POSTS = [
   {
@@ -33,11 +33,41 @@ const USERS = [
 
 function App() {
   return (
-    <>
+    <div>
       <Navbar />
-      <PostCard title="ทดสอบ" body="นี่คือเนื้อหาทดสอบ" />
-    </>
-  )
+      <div
+        style={{
+          maxWidth: "900px",
+          margin: "2rem auto",
+          padding: "0 1rem",
+          display: "grid",
+          gridTemplateColumns: "2fr 1fr",
+          gap: "2rem",
+        }}
+      >
+        {/* คอลัมน์ซ้าย: โพสต์ */}
+        <div>
+          <PostList posts={POSTS} />
+        </div>
+
+        {/* คอลัมน์ขวา: สมาชิก */}
+        <div>
+          <h2
+            style={{
+              color: "#2d3748",
+              borderBottom: "2px solid #1e40af",
+              paddingBottom: "0.5rem",
+            }}
+          >
+            สมาชิก
+          </h2>
+          {USERS.map((user) => (
+            <UserCard key={user.id} name={user.name} email={user.email} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
